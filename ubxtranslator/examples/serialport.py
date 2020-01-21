2182a95c24a92087a74390b410ba6f23e89100bb
rajanski <r.lissewski@parkling.eu>
2020-01-21 16:05:04 +0100

    add script to play with serial port.

--- ubxtranslator/examples/serialport.py
+++ ubxtranslator/examples/serialport.py
@@ -9,14 +9,14 @@ The serial package could easily be replaced with an alternative.
 import serial
 
 from ubxtranslator.core import Parser
-from ubxtranslator.predefined import NAV_CLS, ACK_CLS
+from ubxtranslator.predefined import NAV_CLS, ESF_CLS
 
 
 def run():
-    port = serial.Serial('[your port here]', baudrate=9600, timeout=0.1)
+    port = serial.Serial('/dev/ttyACM2', baudrate=115200, timeout=0.1)
 
     parser = Parser([
-        NAV_CLS, ACK_CLS
+        NAV_CLS, ESF_CLS
     ])
 
     print("Starting to listen for UBX packets")
